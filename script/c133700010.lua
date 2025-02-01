e1a042fc3266eb0e5684c9088b67706bd23cbd76
Jão Kassineiro <195915580+KasinaoCore@users.noreply.github.com>
2025-02-01 01:41:01 -0300

    Add files via upload

---
+++ script/c133700010.lua
@@ -0,0 +1,56 @@
+--Elemental HERO Clay Guardsman (Kasinao)
+local s,id=GetID()
+function s.initial_effect(c)
+	--fusion material
+	c:EnableReviveLimit()
+	Fusion.AddProcMixN(c,false,false,84327329,2)
+	--damage
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_DAMAGE)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e1:SetTarget(s.damtg)
+	e1:SetOperation(s.damop)
+	c:RegisterEffect(e1)
+    --destroy self and opponent
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCategory(CATEGORY_DESTROY)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e2:SetCode(EVENT_BATTLE_DESTROYED)
+	e2:SetCondition(s.condition)
+	e2:SetTarget(s.target)
+	e2:SetOperation(s.operation)
+	c:RegisterEffect(e2)
+end
+s.listed_series={0x3008}
+s.material_setcode={0x8,0x3008}
+function s.damtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	local ct=Duel.GetFieldGroupCount(tp,0,LOCATION_ONFIELD)
+	Duel.SetTargetPlayer(1-tp)
+	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,ct*500)
+end
+function s.damop(e,tp,eg,ep,ev,re,r,rp)
+	local p=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER)
+	local ct=Duel.GetFieldGroupCount(tp,0,LOCATION_ONFIELD)
+	Duel.Damage(p,ct*200,REASON_EFFECT)
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsLocation(LOCATION_GRAVE) and e:GetHandler():IsReason(REASON_BATTLE)
+		and e:GetHandler():GetReasonCard():IsRelateToBattle()
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	local rc=e:GetHandler():GetReasonCard()
+	rc:CreateEffectRelation(e)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,rc,1,0,0)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local rc=e:GetHandler():GetReasonCard()
+	if rc:IsRelateToEffect(e) then
+		Duel.Destroy(rc,REASON_EFFECT)
+	end
+end
