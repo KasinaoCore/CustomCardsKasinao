62e79e002947cbd2a7cd223f4706f97be9a14aa4
Jão Kassineiro <195915580+KasinaoCore@users.noreply.github.com>
2025-09-13 16:32:16 -0300

    Update c133900002.lua

--- script/c133900002.lua
+++ script/c133900002.lua
@@ -39,32 +39,41 @@ end
 
 -- Activation
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
-	local g=Duel.GetTargetCards(e)
+	-- Get the targeted cards
+	local g=e:GetTargetCards()
+	-- A variable to hold the combined level of the monsters
 	local lv_sum=0
 	local tg=Group.CreateGroup()
-	if g:GetCount()>0 then
+	if not g:Is_Empty() then
+		-- Check if the monsters are still on the field and can be tributed
 		for tc in aux.Next(g) do
 			if tc:IsRelateToEffect(e) and tc:IsAbleToGraveAsCost() then
 				tg:AddCard(tc)
 			end
 		end
 	end
-	
-	if tg:GetCount()==0 then return end
 
+	-- If there are no valid targets to tribute, the effect resolves without effect
+	if tg:Is_Empty() then return end
+	
+	-- Get the sum of the levels *before* sending them to the graveyard
 	for tc in aux.Next(tg) do
 		lv_sum=lv_sum+tc:GetLevel()
 	end
 	
+	-- Send the selected monsters to the graveyard as a cost
 	Duel.SendtoGrave(tg,REASON_COST+REASON_RELEASE)
 	
+	-- If the number of monsters tributed is 0, the effect stops here.
 	if lv_sum==0 then return end
 
+	-- Check for space on the field
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
 	
+	-- Select and special summon from the deck
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 	local sp=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp,lv_sum)
-	if sp:GetCount()>0 then
+	if not sp:Is_Empty() then
 		Duel.SpecialSummon(sp,0,tp,tp,false,false,POS_FACEUP)
 	end
 end
