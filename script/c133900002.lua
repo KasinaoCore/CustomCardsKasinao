9fbdc984fc16e676d2e45180627d6e1a283c7cf9
Jão Kassineiro <195915580+KasinaoCore@users.noreply.github.com>
2025-09-13 16:28:40 -0300

    Update c133900002.lua

--- script/c133900002.lua
+++ script/c133900002.lua
@@ -7,55 +7,64 @@ function s.initial_effect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
-	e1:SetCountLimit(1,id) -- Once per turn
-	e1:SetTarget(s.sptg)
-	e1:SetOperation(s.spop)
+	e1:SetCountLimit(1,id)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 end
 
--- Filter: non-Machine Ojamas
+-- Filter for non-Machine Ojama monsters
 function s.filter(c)
-	return c:IsFaceup() and c:IsSetCard(0xf) and not c:IsRace(RACE_MACHINE) and c:IsAbleToGraveAsCost()
+	-- The check for being able to be tributed as cost.
+	return c:IsFaceup() and c:IsSetCard(0xf) and not c:IsRace(RACE_MACHINE)
 end
 
--- Filter: LIGHT Machine with exact level
-function s.spfilter(c,lv,e,tp)
-	return c:IsLevelBelow(lv) and c:IsAttribute(ATTRIBUTE_LIGHT) and c:IsRace(RACE_MACHINE)
-		and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+-- Filter for LIGHT Machine monsters with specific level
+function s.spfilter(c,e,tp,sum_lv)
+	return c:IsLevelBelow(sum_lv) and c:IsAttribute(ATTRIBUTE_LIGHT) and c:IsRace(RACE_MACHINE)
+	and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
--- Targeting: selects any number of non-Machine Ojamas
-function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and s.filter(chkc) end
 	if chk==0 then
-		local g=Duel.GetMatchingGroup(s.filter,tp,LOCATION_MZONE,0,nil)
-		return #g>0 and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		-- Use Duel.IsExistingMatchingCard with the "any number of monsters" check
+		return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_MZONE,0,1,nil)
 	end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
+	Duel.SelectTarget(tp,s.filter,tp,LOCATION_MZONE,0,1,99,nil)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
+	Duel.SetOperationInfo(0,CATEGORY_TO_GRAVE,e:GetTargetCards(),1,tp,0)
 end
 
--- Operation: tribute selected Ojamas, sum levels, special summon LIGHT Machine
-function s.spop(e,tp,eg,ep,ev,re,r,rp)
-	local g=Duel.GetMatchingGroup(s.filter,tp,LOCATION_MZONE,0,nil)
-	if #g==0 then return end
-
-	-- Player selects any number of monsters (1 to all)
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
-	local sg=g:Select(tp,1,#g,nil)
-
-	-- Sum their Levels
-	local lv=0
-	for tc in aux.Next(sg) do
-		lv=lv+tc:GetLevel()
+-- Activation
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetTargetCards(e)
+	local lv_sum=0
+	local tg=Group.CreateGroup()
+	if g:GetCount()>0 then
+		for tc in aux.Next(g) do
+			if tc:IsRelateToEffect(e) and tc:IsAbleToGraveAsCost() then
+				tg:AddCard(tc)
+			end
+		end
 	end
+	
+	if tg:GetCount()==0 then return end
 
-	-- Tribute selected monsters
-	Duel.SendtoGrave(sg,REASON_COST+REASON_RELEASE)
+	for tc in aux.Next(tg) do
+		lv_sum=lv_sum+tc:GetLevel()
+	end
+	
+	Duel.SendtoGrave(tg,REASON_COST+REASON_RELEASE)
+	
+	if lv_sum==0 then return end
 
-	-- Check MZONE space
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
-
-	-- Special Summon from Deck
-	local sp=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_DECK,0,1,1,nil,lv,e,tp)
-	if #sp>0 then
+	
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local sp=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp,lv_sum)
+	if sp:GetCount()>0 then
 		Duel.SpecialSummon(sp,0,tp,tp,false,false,POS_FACEUP)
 	end
 end
