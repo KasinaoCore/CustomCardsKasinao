6ab99aaa18a85854325e50000aec3aea177ee7b1
Jão Kassineiro <195915580+KasinaoCore@users.noreply.github.com>
2025-09-14 14:16:13 -0300

    +cards de teste
    Assault Cannon Beetle (K)

---
+++ script/c133900007.lua
@@ -0,0 +1,36 @@
+--Assault Cannon Beetle (K)
+local s,id=GetID()
+function s.initial_effect(c)
+	--Contact Fusion procedure
+	c:EnableReviveLimit()
+	Fusion.AddProcMix(c,true,true,133900004,133900005,133900006)
+	Fusion.AddContactProc(c,s.contactfil,s.contactop,s.splimit)
+	--damage
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_DAMAGE)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+    e1:SetCountLimit(3,id)
+	e1:SetCost(s.cost)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+end
+s.listed_names={133900004,133900005,133900006,133900009}
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.CheckReleaseGroupCost(tp,nil,1,false,nil,nil) end
+	local sg=Duel.SelectReleaseGroupCost(tp,nil,1,1,false,nil,nil)
+	Duel.Release(sg,REASON_COST)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetTargetPlayer(1-tp)
+	Duel.SetTargetParam(800)
+	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,500)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+	Duel.Damage(p,d,REASON_EFFECT)
+end
