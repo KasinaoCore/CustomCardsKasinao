7f50b77261c064aaf5bda3ee9718e25a362f974a
Jão Kassineiro <195915580+KasinaoCore@users.noreply.github.com>
2025-09-14 12:43:59 -0300

    Alteração no Efeito +Templates de Cards
    Wicked Canon agora está melhor, e be melhor.

--- script/c133700044.lua
+++ script/c133700044.lua
@@ -11,10 +11,10 @@ function s.initial_effect(c)
     e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
     e1:SetCode(EVENT_BATTLE_DAMAGE)
     e1:SetRange(LOCATION_SZONE)
+    e1:SetCountLimit(1)
     e1:SetCondition(s.tgcon)
     e1:SetTarget(s.tgtg)
     e1:SetOperation(s.tgop)
-    e1:SetCountLimit(1,id)
     c:RegisterEffect(e1)
     -- Effect 2: Banish to summon
 	local e2=Effect.CreateEffect(c)
@@ -22,11 +22,19 @@ function s.initial_effect(c)
 	e2:SetType(EFFECT_TYPE_IGNITION)
     e2:SetRange(LOCATION_SZONE)
 	e2:SetCode(EVENT_FREE_CHAIN)
-    e2:SetCountLimit(1,id)
     e2:SetCost(s.cost)
 	e2:SetTarget(s.target)
 	e2:SetOperation(s.activate)
 	c:RegisterEffect(e2)
+    -- Effect 3: Destruction replacement
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_SINGLE)
+	e3:SetCode(EFFECT_DESTROY_REPLACE)
+	e3:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e3:SetRange(LOCATION_SZONE)
+	e3:SetTarget(s.desreptg)
+	e3:SetOperation(s.desrepop)
+	c:RegisterEffect(e3)
 end
 function s.tgcon(e,tp,eg,ep,ev,re,r,rp)
 	return ep==tp
@@ -42,7 +50,6 @@ function s.tgop(e,tp,eg,ep,ev,re,r,rp)
         Duel.SendtoGrave(g,REASON_EFFECT)
     end
 end
-    -- Banish 3 Spells from GY and this card
 function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
     local c=e:GetHandler()
     if chk==0 then return c:IsAbleToRemoveAsCost() and 
@@ -52,21 +59,17 @@ function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
     g:AddCard(c)
     Duel.Remove(g,POS_FACEUP,REASON_COST)
 end
-	--Check for fusion monster with listed material
 function s.exfilter(c,e,tp)
 	return c.material and c:IsType(TYPE_FUSION) and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_HAND+LOCATION_DECK,0,1,nil,e,tp,c)
 end
-	--Check if listed material can be special summoned
 function s.spfilter(c,e,tp,fc)
 	return c:IsCanBeSpecialSummoned(e,0,tp,false,false) and c:IsCode(table.unpack(fc.material))
 end
-	--Activation legality
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 
 		and Duel.IsExistingMatchingCard(s.exfilter,tp,LOCATION_EXTRA,0,1,nil,e,tp) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND+LOCATION_DECK)
 end
-	--Special summon listed material from hand or deck
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CONFIRM)
@@ -78,4 +81,26 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	if #g>0 then
 		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
 	end
+end
+function s.desreptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return not c:IsReason(REASON_REPLACE) and c:IsOnField() and c:IsFaceup()
+		and Duel.IsExistingMatchingCard(s.repfilter,tp,LOCATION_SZONE,0,1,c,e) end
+	if Duel.SelectEffectYesNo(tp,c,96) then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESREPLACE)
+		local g=Duel.SelectMatchingCard(tp,s.repfilter,tp,LOCATION_SZONE,0,1,1,c,e)
+		e:SetLabelObject(g:GetFirst())
+		Duel.HintSelection(g)
+		g:GetFirst():SetStatus(STATUS_DESTROY_CONFIRMED,true)
+		return true
+	else return false end
+end
+function s.desrepop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=e:GetLabelObject()
+	tc:SetStatus(STATUS_DESTROY_CONFIRMED,false)
+	Duel.Destroy(tc,REASON_EFFECT|REASON_REPLACE)
+end
+function s.repfilter(c,e)
+	return c:GetSequence()<5 and c:IsDestructable(e)
+		and not c:IsStatus(STATUS_DESTROY_CONFIRMED+STATUS_BATTLE_DESTROYED)
 end
