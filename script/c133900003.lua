709dc57ad52e6bcb070bf88903fa1546148cef41
Jão Kassineiro <195915580+KasinaoCore@users.noreply.github.com>
2025-09-15 14:33:51 -0300

    Ojamamamamama

--- script/c133900003.lua
+++ script/c133900003.lua
@@ -13,48 +13,65 @@ function s.initial_effect(c)
 	c:RegisterEffect(e1)
 end
 s.listed_series={SET_OJAMA}
+
 function s.costfilter(c)
-	return c:IsDiscardable() and c:IsAbleToGraveAsCost() and c:IsSetCard(SET_OJAMA) and c:IsMonsterCard()
+	return c:IsDiscardable() and c:IsAbleToGraveAsCost() and c:IsSetCard(SET_OJAMA) and c:IsMonster()
 end
-function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
-	local g=Duel.GetMatchingGroup(s.costfilter,tp,LOCATION_HAND,0,nil)
-	if chk==0 then return aux.SelectUnselectGroup(g,e,tp,1,3,aux.dncheck,0) end
-	local cg=aux.SelectUnselectGroup(g,e,tp,1,3,aux.dncheck,1,tp,HINTMSG_DISCARD)
-	local ct=Duel.SendtoGrave(cg,REASON_COST+REASON_DISCARD)
-	e:SetLabel(ct)
-end
+
 function s.filter(c,e,tp)
 	return c:IsLevelBelow(4) and c:IsRace(RACE_MACHINE) and c:IsType(TYPE_UNION)
 		and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP_DEFENSE)
 end
+
+function s.activation_legality(tp,e)
+	local g=Duel.GetMatchingGroup(s.filter,tp,LOCATION_DECK,0,nil,e,tp)
+	local codes={}
+	for tc in aux.Next(g) do
+		codes[tc:GetCode()]=true
+	end
+	local count=0
+	for _ in pairs(codes) do count=count+1 end
+	return count
+end
+
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	local max_summon=math.min(3,s.activation_legality(tp,e))
+	local g=Duel.GetMatchingGroup(s.costfilter,tp,LOCATION_HAND,0,nil)
+	if chk==0 then return aux.SelectUnselectGroup(g,e,tp,1,max_summon,aux.dncheck,0) end
+	local cg=aux.SelectUnselectGroup(g,e,tp,1,max_summon,aux.dncheck,1,tp,HINTMSG_DISCARD)
+	local ct=Duel.SendtoGrave(cg,REASON_COST+REASON_DISCARD)
+	e:SetLabel(ct)
+end
+
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
 		and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_DECK,0,1,nil,e,tp) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
 end
+
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
-	local ft=math.min(Duel.GetLocationCount(tp,LOCATION_MZONE),3)
+	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
 	if ft<=0 then return end
 	if Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) then ft=1 end
 	local ct=math.min(e:GetLabel(),ft)
 	if ct<=0 then return end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+
 	local g=Duel.GetMatchingGroup(s.filter,tp,LOCATION_DECK,0,nil,e,tp)
+	if g:GetCount()==0 then return end
+
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 	local sg=aux.SelectUnselectGroup(g,e,tp,ct,ct,function(selected)
-		local names = {}
-		for sc in selected:Iter() do
-			if names[sc:GetCode()] then
-				return false
-			end
-			names[sc:GetCode()] = true
+		local codes={}
+		for tc in aux.Next(selected) do
+			if codes[tc:GetCode()] then return false end
+			codes[tc:GetCode()]=true
 		end
 		return true
 	end,1,tp,HINTMSG_SPSUMMON)
 
 	local c=e:GetHandler()
-	for tc in sg:Iter() do
+	for tc in aux.Next(sg) do
 		if Duel.SpecialSummonStep(tc,0,tp,tp,false,false,POS_FACEUP_DEFENSE) then
-			-- Cannot change its battle position
 			local e1=Effect.CreateEffect(c)
 			e1:SetDescription(3313)
 			e1:SetType(EFFECT_TYPE_SINGLE)
