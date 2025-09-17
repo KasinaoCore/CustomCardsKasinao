6db73f7954be9f1b57f6f488b7c671314a809d88
Jão Kassineiro <195915580+KasinaoCore@users.noreply.github.com>
2025-09-17 13:08:57 -0300

    Update c133900002.lua

--- script/c133900002.lua
+++ script/c133900002.lua
@@ -1,37 +1,39 @@
+--Mecha Ojama King Transformation (K)
 local s,id=GetID()
 function s.initial_effect(c)
-	-- Activate
-	local e1=Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
-	e1:SetType(EFFECT_TYPE_ACTIVATE)
-	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetCountLimit(1,id)
-	e1:SetCost(s.cost)
-	e1:SetTarget(s.sptg)
-	e1:SetOperation(s.spop)
-	c:RegisterEffect(e1)
+    -- Activate
+    local e1=Effect.CreateEffect(c)
+    e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+    e1:SetType(EFFECT_TYPE_ACTIVATE)
+    e1:SetCode(EVENT_FREE_CHAIN)
+    e1:SetCountLimit(1,id)
+    e1:SetCost(s.cost)
+    e1:SetTarget(s.sptg)
+    e1:SetOperation(s.spop)
+    c:RegisterEffect(e1)
 end
 function s.tribute_filter(c)
-	return c:IsSetCard(0xf) and c:IsType(TYPE_MONSTER) and not c:IsRace(RACE_MACHINE) 
+    return c:IsSetCard(0xf) and c:IsType(TYPE_MONSTER) and not c:IsRace(RACE_MACHINE) 
 end
 function s.sp_filter(c,e,tp)
-	return c:IsRace(RACE_MACHINE) and c:IsAttribute(ATTRIBUTE_LIGHT) and c:IsLevelBelow(6)
-		and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+    return c:IsRace(RACE_MACHINE) and c:IsAttribute(ATTRIBUTE_LIGHT) and c:IsLevelBelow(6)
+        and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
-	local g=Duel.GetMatchingGroup(s.tribute_filter,tp,LOCATION_MZONE,0,nil)
-	if chk==0 then return g:CheckWithSumEqual(Card.GetLevel,6,1,99) end
-	local tg=g:SelectWithSumEqual(tp,Card.GetLevel,6,1,99)
-	Duel.Release(tg,REASON_COST)
+    local g=Duel.GetMatchingGroup(s.tribute_filter,tp,LOCATION_MZONE+LOCATION_HAND,0,nil)
+    if chk==0 then return g:CheckWithSumEqual(Card.GetLevel,6,1,99) end
+    local tg=g:SelectWithSumEqual(tp,Card.GetLevel,6,1,99)
+    Duel.Release(tg,REASON_COST)
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.sp_filter,tp,LOCATION_DECK,0,1,nil,e,tp) end
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
+    if chk==0 then return Duel.IsExistingMatchingCard(s.sp_filter,tp,LOCATION_DECK,0,1,nil,e,tp) end
+    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
 end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local sg=Duel.SelectMatchingCard(tp,s.sp_filter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
-	if #sg>0 then
-		Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_FACEUP)
-	end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+    local sg=Duel.SelectMatchingCard(tp,s.sp_filter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
+    if #sg>0 then
+        Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_FACEUP)
+
+    end
 end
