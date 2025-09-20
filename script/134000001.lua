9eede4ba9a0cee1a8326450e29147e82b255a3a8
Jão Kassineiro <195915580+KasinaoCore@users.noreply.github.com>
2025-09-20 14:30:47 -0300

    Babuíno Brabo Adicionado

---
+++ script/134000001.lua
@@ -0,0 +1,64 @@
+--Metal Baboon, Stalwart of the Forest (K)
+local s,id=GetID()
+function s.initial_effect(c)
+	c:EnableReviveLimit()
+	Link.AddProcedure(c,s.matfilter,2,2)
+    --Destruction replacement
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1:SetCode(EFFECT_DESTROY_REPLACE)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetTarget(s.reptg)
+	e1:SetValue(s.repval)
+	c:RegisterEffect(e1)
+	--spsummon
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e2:SetRange(LOCATION_GRAVE)
+	e2:SetCode(EVENT_TO_GRAVE)
+    e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_DUEL)
+	e2:SetCondition(s.condition)
+	e2:SetCost(Cost.PayLP(1000))
+	e2:SetTarget(s.target)
+	e2:SetOperation(s.operation)
+	c:RegisterEffect(e2)
+end
+function s.matfilter(c,lc,sumtype,tp)
+	return c:IsLevelAbove(5) and c:IsRace(RACE_BEAST,lc,sumtype,tp)
+end
+function s.repfilter(c,tp,lg)
+	return lg:IsContains(c) and c:IsControler(tp) and c:IsLocation(LOCATION_MZONE)
+		and (c:IsReason(REASON_BATTLE) or (c:IsReason(REASON_EFFECT) and c:GetReasonPlayer()==1-tp))
+end
+function s.reptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local lg=e:GetHandler():GetLinkedGroup()
+    local ct=eg:FilterCount(s.repfilter,nil,tp,lg)
+	if chk==0 then return ct>0 and Duel.CheckLPCost(tp,1000*ct) end
+	if Duel.SelectEffectYesNo(tp,e:GetHandler(),96) then
+		Duel.PayLPCost(tp,1000*ct)
+		return true
+	else return false end
+end
+function s.repval(e,c)
+	return s.repfilter(c,e:GetHandlerPlayer(),e:GetHandler():GetLinkedGroup())
+end
+--spsummon
+function s.cfilter(c,tp)
+	return c:IsMonster() and c:IsRace(RACE_BEAST) and c:IsPreviousControler(tp) and c:IsPreviousPosition(POS_FACEUP)
+		and c:IsPreviousLocation(LOCATION_MZONE) and (c:GetPreviousRaceOnField()&RACE_BEAST)~=0
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return not eg:IsContains(e:GetHandler()) and eg:IsExists(s.cfilter,1,nil,tp) and (r&REASON_DESTROY)~=0
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if not c:IsRelateToEffect(e) then return end
+	Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
+end
