896633ba8410cfd9c317a3645b93ca431a94ac46
Jão Kassineiro <195915580+KasinaoCore@users.noreply.github.com>
2025-09-22 13:35:37 -0300

    Erratas PSCT e funcionais
    Algumas erratas PSCT e buff no Mecha Ojama king para ficar tier 0

--- script/c133900001.lua
+++ script/c133900001.lua
@@ -10,16 +10,24 @@ function s.initial_effect(c)
 	e1:SetCode(EFFECT_SPSUMMON_CONDITION)
 	e1:SetValue(s.splimit)
 	c:RegisterEffect(e1)
-	-- Once per turn: Special Summon 1 Level 4 or lower LIGHT Machine from hand or Deck
+	-- Inflict 300 damage to your opponent's LP for each Ojama destroyed by battle
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(id,0))
-	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
-	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e2:SetCode(EVENT_BATTLE_DESTROYED)
 	e2:SetRange(LOCATION_MZONE)
-	e2:SetCountLimit(1,id)
-	e2:SetTarget(s.sptg)
-	e2:SetOperation(s.spop)
+	e2:SetCondition(s.damcon)
+	e2:SetOperation(s.damop)
 	c:RegisterEffect(e2)
+	-- Once per turn: Special Summon 1 Level 4 or lower LIGHT Machine or 1 "Ojama" from hand or Deck
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,0))
+	e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e3:SetType(EFFECT_TYPE_IGNITION)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCountLimit(1,id)
+	e3:SetTarget(s.sptg)
+	e3:SetOperation(s.spop)
+	c:RegisterEffect(e3)
 end
 
 -- Must be Special Summoned by CARD EFFECT!!!! I think.
@@ -27,10 +35,11 @@ function s.splimit(e,se,sp,st)
 	return se~=nil
 end
 
--- Filter for Level 4 or lower LIGHT Machine
+-- Filter for Level 4 or lower LIGHT Machine or 1 "Ojama"
 function s.spfilter(c,e,tp)
-	return c:IsLevelBelow(4) and c:IsAttribute(ATTRIBUTE_LIGHT) and c:IsRace(RACE_MACHINE)
+	return (c:IsLevelBelow(4) and c:IsAttribute(ATTRIBUTE_LIGHT) and c:IsRace(RACE_MACHINE)) or c:IsSetCard(SET_OJAMA)
 		and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+		and not c:IsCode(id)
 end
 
 -- The target to summon (I think?)
@@ -49,5 +58,20 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_HAND+LOCATION_DECK,0,1,1,nil,e,tp)
 	if #g>0 then
 		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
+
+-- Damage effect
+function s.ctfilter(c, tp)
+	return c:IsPreviousLocation(LOCATION_MZONE) and c:IsPreviousControler(tp) and c:IsPreviousSetCard(SET_OJAMA)
+end
+function s.damcon(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(s.ctfilter,1,nil,tp)
+end
+-- Operation: burn the opponent!
+function s.damop(e,tp,eg,ep,ev,re,r,rp)
+	local ct=eg:FilterCount(s.ctfilter,nil,tp)
+	if ct>0 then
+		Duel.Damage(1-tp,ct*300,REASON_EFFECT)
 	end
 end
