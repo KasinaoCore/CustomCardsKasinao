8c4afab6fa8822722a80ffe986b028bc13e1171d
Jão Kassineiro <195915580+KasinaoCore@users.noreply.github.com>
2025-09-29 00:40:18 -0300

    Dark Wave consertada
    Eu nem sabia que tava bugado até testar agora

--- script/c133700013.lua
+++ script/c133700013.lua
@@ -23,7 +23,7 @@ function s.initial_effect(c)
 end
 s.listed_series={0x600}
 function s.filter(c)
-	return c:IsFaceup() and c:HasLevel() and c:IsSetCard(0x600) or (not c:IsType(TYPE_TUNER))
+	return c:IsFaceup() and c:HasLevel() and (c:IsSetCard(0x600) or not c:IsType(TYPE_TUNER))
 end
 function s.lvtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and s.filter(chkc) end
