18a0f77cc961eec143397ae89789b32b41ab4c65
Jão Kassineiro <195915580+KasinaoCore@users.noreply.github.com>
2025-10-07 00:43:12 -0300

    Buff Tribulldog
    Tribulldog (K)
    Agora ele absurdo de quebrado.
    Muda o nível de todas as bestas de nível 4 ou menos e ainda é quando ele é invocado por invocação especial por qualquer método, além de invocar tribulldogs da mão no estilo do anime. (Uma vez por turno, invoca 1 "Tribulldog" da mão)

--- script/c134000002.lua
+++ script/c134000002.lua
@@ -7,7 +7,8 @@ function s.initial_effect(c)
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e1:SetType(EFFECT_TYPE_IGNITION)
 	e1:SetRange(LOCATION_MZONE)
-	e1:SetCountLimit(1,id)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCountLimit(1)
 	e1:SetTarget(s.sptg)
 	e1:SetOperation(s.spop)
 	c:RegisterEffect(e1)
@@ -18,45 +19,30 @@ function s.initial_effect(c)
 	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e2:SetProperty(EFFECT_FLAG_DELAY)
 	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
-	e2:SetCountLimit(1,{id,1})
-	e2:SetCondition(s.lvcon)
+	e2:SetCountLimit(1,id)
 	e2:SetTarget(s.lvtg)
 	e2:SetOperation(s.lvop)
 	c:RegisterEffect(e2)
-
-	Duel.AddCustomActivityCounter(id,ACTIVITY_SUMMON,s.counterfilter)
-	Duel.AddCustomActivityCounter(id,ACTIVITY_SPSUMMON,s.counterfilter)
 end
+
 s.listed_names={id}
-
-function s.counterfilter(c)
-	return c:IsRace(RACE_BEAST)
-end
 
 function s.spfilter(c,e,tp)
 	return c:IsCode(id) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then
-		return Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_HAND,0,1,nil,e,tp)
-	end
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_HAND,0,1,nil,e,tp) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
 end
-
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
-	if ft<=0 then return end
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_HAND,0,1,ft,nil,e,tp)
+	local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_HAND,0,1,1,nil,e,tp)
 	if #g>0 then
 		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
 	end
-end
-
-function s.lvcon(e,tp,eg,ep,ev,re,r,rp)
-	return re and re:GetHandler():IsCode(id)
 end
 
 function s.lvtg(e,tp,eg,ep,ev,re,r,rp,chk)
@@ -67,7 +53,7 @@ function s.lvtg(e,tp,eg,ep,ev,re,r,rp,chk)
 end
 
 function s.lvfilter(c)
-	return c:IsFaceup() and c:IsCode(id)
+	return c:IsLevelBelow(4) and c:IsRace(RACE_BEAST) and c:IsFaceup()
 end
 
 function s.lvop(e,tp,eg,ep,ev,re,r,rp)
@@ -81,17 +67,4 @@ function s.lvop(e,tp,eg,ep,ev,re,r,rp)
 		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
 		tc:RegisterEffect(e1)
 	end
-
-	local e2=Effect.CreateEffect(e:GetHandler())
-	e2:SetType(EFFECT_TYPE_FIELD)
-	e2:SetCode(EFFECT_CANNOT_SUMMON)
-	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-	e2:SetTargetRange(1,0)
-	e2:SetTarget(function(_,c) return not c:IsRace(RACE_BEAST) end)
-	e2:SetReset(RESET_PHASE+PHASE_END)
-	Duel.RegisterEffect(e2,tp)
-	local e3=e2:Clone()
-	e3:SetCode(EFFECT_CANNOT_SUMMON)
-	Duel.RegisterEffect(e3,tp)
-	aux.RegisterClientHint(e:GetHandler(),nil,tp,1,0,aux.Stringid(id,2),nil)
 end
