b06f7d09026f3a1486a263d4903dd274902b6c10
Jão Kassineiro <195915580+KasinaoCore@users.noreply.github.com>
2025-10-14 00:30:46 -0300

    Update c1337694269.lua

--- script/c1337694269.lua
+++ script/c1337694269.lua
@@ -31,17 +31,17 @@ function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetFlagEffect(ep,id)>0 then return false end
 	return aux.CanActivateSkill(tp)
 		and Duel.IsMainPhase()
-		and Duel.CheckLPCost(tp,100)
+		and Duel.CheckLPCost(tp,1000)
 		and Duel.IsExistingMatchingCard(s.canfilter,tp,LOCATION_DECK,0,1,nil)
 end
 
 function s.flipop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SKILL_FLIP,tp,id|(1<<32))
 	Duel.Hint(HINT_CARD,tp,id)
-	Duel.RegisterFlagEffect(ep,id,RESET_PHASE+PHASE_END,0,1) -- once per turn
+	Duel.RegisterFlagEffect(ep,id,RESET_PHASE+PHASE_END,0,1)
 
-	if Duel.CheckLPCost(tp,100) then
-		Duel.PayLPCost(tp,100)
+	if Duel.CheckLPCost(tp,1000) then
+		Duel.PayLPCost(tp,1000)
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
 		local g=Duel.SelectMatchingCard(tp,s.canfilter,tp,LOCATION_DECK,0,1,1,nil)
 		if #g>0 then
