15599c4ec0bf424ff1af7c3430a3cd36b3df5f8e
Jão Kassineiro <195915580+KasinaoCore@users.noreply.github.com>
2025-10-14 00:09:47 -0300

    ????

--- script/c1337.lua
+++ script/c1337.lua
@@ -26,5 +26,38 @@ KasinaoArchetype.Drone = {
 	4474060, -- SPYRAL Gear - Drone
 	24610207 -- Star Drawing
 }
+KasinaoArchetype.Can = {
+	62651957,
+	5861892,
+	69831560,
+	23846921,
+	39761418,
+	97452817,
+	59712426,
+	60953118,
+	1876841,
+	34568403,
+	97574404,
+	3376703,
+	61175706,
+	35781051,
+	45159319,
+	8396952,
+	95614612,
+	62892347,
+	75198893,
+	8251996,
+	11819473,
+	32062913,
+	76302448,
+	77066768,
+	133700044,
+	3492538,
+	28120197,
+	25096909,
+	70916046
+}
 
 Card.IsKasinaoDrone = MakeCheck({0x803}, KasinaoArchetype.Drone)
+Card.IsKasinaoCan = MakeCheck(nil, KasinaoArchetype.Can)
+
