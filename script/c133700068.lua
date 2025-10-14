55b612b6a33aceb3245e32245cff7c51ca7c80aa
Jão Kassineiro <195915580+KasinaoCore@users.noreply.github.com>
2025-10-14 13:56:11 -0300

    Update c133700068.lua

--- script/c133700068.lua
+++ script/c133700068.lua
@@ -3,30 +3,35 @@ local s,id=GetID()
 function s.initial_effect(c)
 	aux.AddEquipProcedure(c)
 	--Increase ATK
-	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_EQUIP)
-	e2:SetCode(EFFECT_UPDATE_ATTACK)
-	e2:SetValue(s.value)
-	c:RegisterEffect(e2)
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_EQUIP)
+	e1:SetCode(EFFECT_UPDATE_ATTACK)
+	e1:SetValue(s.value)
+	c:RegisterEffect(e1)
 	--Search Deck
-	local e4=Effect.CreateEffect(c)
-	e4:SetDescription(aux.Stringid(id,0))
-	e4:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
-	e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e4:SetCode(EVENT_TO_GRAVE)
-	e4:SetProperty(EFFECT_FLAG_DELAY)
-	e4:SetTarget(s.stg)
-	e4:SetOperation(s.sop)
-	c:RegisterEffect(e4)
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_TO_GRAVE)
+	e2:SetProperty(EFFECT_FLAG_DELAY)
+	e2:SetTarget(s.stg)
+	e2:SetCondition(s.scon)
+	e2:SetOperation(s.sop)
+	c:RegisterEffect(e2)
 end
 s.listed_names={134000002}
+function s.vfilter(c)
+    return c:IsRace(RACE_BEAST) and (c:IsLevelBelow(4) or c:IsRankBelow(4))
+end
 function s.value(e,c)
 	return Duel.GetMatchingGroupCount(s.vfilter,e:GetHandlerPlayer(),LOCATION_MZONE,0,nil)*500
 end
-function s.vfilter(c)
-    return c:IsRace(RACE_BEAST) and (c:IsLevelBelow(4) or c:IsRankBelow(4))
 function s.sfilter(c)
 	return c:IsCode(134000002) and c:IsAbleToHand()
+end
+function s.scon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsPreviousLocation(LOCATION_ONFIELD)
 end
 function s.stg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chk==0 then return Duel.IsExistingMatchingCard(s.sfilter,tp,LOCATION_DECK,0,1,nil) end
