790802ca65eebc3127fdfad7044b7b0ee3796589
Jão Kassineiro <195915580+KasinaoCore@users.noreply.github.com>
2025-10-19 17:51:46 -0300

    Magnet Fusion refeita

--- script/c133700065.lua
+++ script/c133700065.lua
@@ -1,49 +1,22 @@
 --Magnet Fusion (K)
+Duel.LoadScript("c1337.lua")
 local s,id=GetID()
 function s.initial_effect(c)
-	--Special summon 1 Magnedragon from extra deck
-	local e1=Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_FUSION_SUMMON)
-	e1:SetType(EFFECT_TYPE_ACTIVATE)
-	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetCost(s.cost)
-	e1:SetTarget(s.target)
-	e1:SetOperation(s.activate)
+	--Activate
+	local e1=Fusion.CreateSummonEff
+	{handler=c,fusfilter=s.fusfilter,matfilter=s.matfilter,extrafil=s.fextra,extraop=Fusion.ReleaseMaterial,extratg=s.extratg}
 	c:RegisterEffect(e1)
 end
-
-function s.costfilter(c)
-	return c:IsRace(RACE_ROCK) and c:IsReleasable()
+function s.fusfilter(c)
+	return c:IsType(TYPE_FUSION)
 end
-function s.filter(c,e,tp)
-	return c:IsType(TYPE_FUSION) and c:IsCode(133700064) and Duel.GetLocationCountFromEx(tp,tp,nil,c)>0
-		and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_FUSION,tp,false,false) and c:CheckFusionMaterial()
+function s.matfilter(c,e,tp)
+	return c:IsRace(RACE_ROCK) and c:IsReleasableByEffect()
 end
-function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then 
-	return Duel.CheckReleaseGroupCost(tp,s.costfilter,3,false,aux.ReleaseCheckMMZ,nil) end
-	local g=Duel.SelectReleaseGroupCost(tp,s.costfilter,3,3,false,aux.ReleaseCheckMMZ,nil)
-	Duel.Release(g,REASON_COST)
+function s.fextra(e,tp,mg)
+	return Duel.GetMatchingGroup(Fusion.IsMonsterFilter(Card.IsReleasableByEffect),tp,LOCATION_HAND|LOCATION_MZONE,0,nil)
 end
-function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_EXTRA,0,1,nil,e,tp) end
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
-end
-function s.activate(e,tp,eg,ep,ev,re,r,rp)
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_EXTRA,0,1,1,nil,e,tp)
-	local tc=g:GetFirst()
-	if not tc then return end
-	tc:SetMaterial(nil)
-	if Duel.SpecialSummon(tc,SUMMON_TYPE_FUSION,tp,tp,false,false,POS_FACEUP)~=0 then
-		--Cannot be targeted by opponent's card effects
-		local e1=Effect.CreateEffect(e:GetHandler())
-		e1:SetDescription(3061)
-		e1:SetProperty(EFFECT_FLAG_CLIENT_HINT)
-		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
-		e1:SetReset(RESETS_STANDARD_PHASE_END)
-		e1:SetValue(aux.tgoval)
-		tc:RegisterEffect(e1)
-	end
+function s.extratg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_RELEASE,nil,1,tp,LOCATION_HAND|LOCATION_MZONE)
 end
