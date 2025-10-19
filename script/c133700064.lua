3633c182db9d4b61bc65daa88563dbe031184d58
Jão Kassineiro <195915580+KasinaoCore@users.noreply.github.com>
2025-10-19 18:38:45 -0300

    Update c133700064.lua
    Nerfzinho no Magnedragon pra brickar mais

--- script/c133700064.lua
+++ script/c133700064.lua
@@ -35,7 +35,7 @@ function s.initial_effect(c)
 	c:RegisterEffect(e3)
 end
 function s.ffilter(c,fc,sumtype,tp,sub,mg,sg)
-	return c:IsRace(RACE_ROCK,fc,sumtype,tp) and (not sg or not sg:IsExists(s.fusfilter,1,c,c:GetCode(fc,sumtype,tp),fc,sumtype,tp))
+	return (c:IsLevel(4) and c:IsRace(RACE_ROCK,fc,sumtype,tp)) and (not sg or not sg:IsExists(s.fusfilter,1,c,c:GetCode(fc,sumtype,tp),fc,sumtype,tp))
 end
 function s.fusfilter(c,code,fc,sumtype,tp)
 	return c:IsSummonCode(fc,sumtype,tp,code) and not c:IsHasEffect(511002961)
