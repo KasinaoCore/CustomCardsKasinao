81e76bde21566bd1b833c86e3f158cefae8b22de
Jão Kassineiro <195915580+KasinaoCore@users.noreply.github.com>
2025-10-19 18:12:38 -0300

    Mesmo de antes

--- script/c133700065.lua
+++ script/c133700065.lua
@@ -3,9 +3,7 @@ Duel.LoadScript("c1337.lua")
 local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
-	local e1=Fusion.CreateSummonEff
-	{handler=c,fusfilter=s.fusfilter,matfilter=s.matfilter,extrafil=s.fextra,extraop=Fusion.ReleaseMaterial,extratg=s.extratg}
-	c:RegisterEffect(e1)
+	c:RegisterEffect(Fusion.CreateSummonEff({handler=c,fusfilter=s.fusfilter,matfilter=s.matfilter,extrafil=s.fextra,extraop=Fusion.ReleaseMaterial,extratg=s.extratg,stage2=s.stage2}))
 end
 function s.fusfilter(c)
 	return c:IsType(TYPE_FUSION)
@@ -19,4 +17,20 @@ end
 function s.extratg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
 	Duel.SetOperationInfo(0,CATEGORY_RELEASE,nil,1,tp,LOCATION_HAND|LOCATION_MZONE)
+end
+function s.atklimit(e,c)
+	return c:IsControler(1-e:GetHandlerPlayer())
+end
+function s.stage2(e,tc,tp,sg,chk)
+	if chk==1 then
+		--Cannot be targeted by opponent's card effects
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetDescription(3061)
+		e1:SetProperty(EFFECT_FLAG_CLIENT_HINT)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
+		e1:SetReset(RESETS_STANDARD_PHASE_END)
+		e1:SetValue(aux.tgoval)
+		tc:RegisterEffect(e1)
+	end
 end
