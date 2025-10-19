65067cc00aedf068059d081de5d844bee7db7518
Jão Kassineiro <195915580+KasinaoCore@users.noreply.github.com>
2025-10-19 18:55:50 -0300

    Efeito do Electromagnedragon Relectromagnefeito
    Efeito do Electromagnedragon Relectromagnefeito

--- script/--Electromagnetic Magnedragon (K) OLD.lua
+++ script/--Electromagnetic Magnedragon (K) OLD.lua
@@ -1,4 +1,4 @@
---Electromagnetic Magnedragon (K)
+--Electromagnetic Magnedragon (K) OLD
 local s,id=GetID()
 function s.initial_effect(c)
 	c:EnableReviveLimit()
@@ -21,6 +21,18 @@ function s.initial_effect(c)
 	e2:SetTarget(s.damtg)
 	e2:SetOperation(s.damop)
 	c:RegisterEffect(e2)
+	--Special Summon 3 Level 4 or lower Rock monsters from your GY
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,1))
+	e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e3:SetProperty(EFFECT_FLAG_DELAY)
+	e3:SetCode(EVENT_DESTROYED)
+	e3:SetCountLimit(1)
+	e3:SetCondition(s.gyspcon)
+	e3:SetTarget(s.gysptg)
+	e3:SetOperation(s.gyspop)
+	c:RegisterEffect(e3)
 end
 function s.ffilter(c,fc,sumtype,tp,sub,mg,sg)
 	return (c:IsLevel(4) and c:IsRace(RACE_ROCK,fc,sumtype,tp)) and (not sg or not sg:IsExists(s.fusfilter,1,c,c:GetCode(fc,sumtype,tp),fc,sumtype,tp))
@@ -50,6 +62,26 @@ function s.damop(e,tp,eg,ep,ev,re,r,rp)
 		local dam=tc:GetAttack()
 		if dam<0 then dam=0 end
 		Duel.Damage(p,dam,REASON_EFFECT)
+	end
+end
+function s.gyspfilter(c,e,tp)
+	return c:IsLevelBelow(4) and c:IsRace(RACE_ROCK) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.gyspcon(e,tp,eg,ep,ev,re,r,rp)
+	return (r&REASON_EFFECT+REASON_BATTLE)~=0
+end
+function s.gysptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return not Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT)
+		and Duel.GetLocationCount(tp,LOCATION_MZONE)>2
+		and Duel.IsExistingMatchingCard(s.gyspfilter,tp,LOCATION_GRAVE,0,3,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,3,tp,LOCATION_GRAVE)
+end
+function s.gyspop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) or Duel.GetLocationCount(tp,LOCATION_MZONE)<=1 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,s.gyspfilter,tp,LOCATION_GRAVE,0,3,3,nil,e,tp)
+	if #g>1 then
+		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEDOWN_DEFENSE)
 	end
 end
 
