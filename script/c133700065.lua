70e0cce33ff4a93213d6e507f2b6f170462337c2
Jão Kassineiro <195915580+KasinaoCore@users.noreply.github.com>
2025-10-20 14:01:12 -0300

    Buff na magnet fusion
    Agora precisa de só UMA rocha.

--- script/c133700065.lua
+++ script/c133700065.lua
@@ -3,21 +3,24 @@ Duel.LoadScript("c1337.lua")
 local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
-	c:RegisterEffect(Fusion.CreateSummonEff({handler=c,fusfilter=s.fusfilter,matfilter=s.matfilter,extrafil=s.fextra,extraop=Fusion.ReleaseMaterial,extratg=s.extratg,stage2=s.stage2}))
+	c:RegisterEffect(Fusion.CreateSummonEff({handler=c,fusfilter=s.fusfilter,matfilter=s.matfilter,extrafil=s.fextra,extraop=Fusion.ReleaseMaterial,extratg=s.extratg,stage2=s.stage2,checkmat=s.checkmat}))
 end
 function s.fusfilter(c)
 	return c:IsType(TYPE_FUSION)
 end
 function s.matfilter(c,e,tp)
-	return c:IsRace(RACE_ROCK) and c:IsReleasableByEffect()
+	return c:IsReleasableByEffect()
 end
 function s.fextra(e,tp,mg)
-	return Duel.GetMatchingGroup(Fusion.IsMonsterFilter(Card.IsReleasableByEffect),tp,LOCATION_HAND|LOCATION_MZONE,0,nil)
+	return Duel.GetMatchingGroup(Fusion.IsMonsterFilter(Card.IsReleasableByEffect),tp,LOCATION_HAND|LOCATION_MZONE,0,nil),s.checkmat
 end
 function s.extratg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
 	Duel.SetOperationInfo(0,CATEGORY_RELEASE,nil,1,tp,LOCATION_HAND|LOCATION_MZONE)
 end
+function s.checkmat(tp,sg,fc)
+	return sg:IsExists(Card.IsRace,1,nil,RACE_ROCK)
+end	
 function s.atlimit(e,c)
 	return c~=e:GetHandler()
 end
