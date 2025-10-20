4a08e247692b108035f0f01c4138dfb3ec6b2837
Jão Kassineiro <195915580+KasinaoCore@users.noreply.github.com>
2025-10-20 17:38:35 -0300

    Buff na Gate Guardian Ritual
    Adicionado um negate soft OPT

--- script/c133700003.lua
+++ script/c133700003.lua
@@ -1,4 +1,4 @@
---Rite of the Gate Guardian(Kasinao)
+--Gate Guardian Ritual (K)
 local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
@@ -26,7 +26,7 @@ function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	Duel.Remove(g,POS_FACEDOWN,REASON_COST)
 end
 function s.spfilter(c,e,tp)
-	return c:IsLevelBelow(11) and c:IsSetCard(SET_GATE_GUARDIAN) and c:IsCanBeSpecialSummoned(e,0,tp,true,false) and (c:IsLocation(LOCATION_DECK) or c:IsLocation(LOCATION_HAND))
+	return c:IsCode(25833572) and c:IsCanBeSpecialSummoned(e,0,tp,true,false) and (c:IsLocation(LOCATION_DECK) or c:IsLocation(LOCATION_HAND))
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_DECK+LOCATION_HAND,0,1,nil,e,tp) end
@@ -37,6 +37,45 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 	local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_DECK+LOCATION_HAND,0,1,1,nil,e,tp)
 	if #g>0 then
-		Duel.SpecialSummon(g,0,tp,tp,true,false,POS_FACEUP)
+		local tc=g:GetFirst()
+		if Duel.SpecialSummon(tc,0,tp,tp,true,false,POS_FACEUP)>0 then
+			--Negate Effect
+			local e1gg=Effect.CreateEffect(e:GetHandler())
+			e1gg:SetProperty(EFFECT_FLAG_CLIENT_HINT)
+			e1gg:SetDescription(aux.Stringid(id,1))
+			e1gg:SetCategory(CATEGORY_DISABLE+CATEGORY_DESTROY)
+			e1gg:SetType(EFFECT_TYPE_QUICK_O)
+			e1gg:SetCode(EVENT_CHAINING)
+			e1gg:SetRange(LOCATION_MZONE)
+			e1gg:SetReset(RESET_EVENT|RESETS_STANDARD)
+			e1gg:SetCountLimit(1)
+			e1gg:SetCondition(s.discon)
+			e1gg:SetTarget(s.distg)
+			e1gg:SetOperation(s.disop)
+			tc:RegisterEffect(e1gg)
+			aux.DoubleSnareValidity(c,LOCATION_MZONE)
+		end
+	end
+end
+function s.tfilter(c,tp)
+	return c:IsOnField() and c:IsControler(tp)
+end
+
+function s.discon(e,tp,eg,ep,ev,re,r,rp)
+	if not (rp==1-tp and re:IsHasProperty(EFFECT_FLAG_CARD_TARGET)) then return false end
+	local tg=Duel.GetChainInfo(ev,CHAININFO_TARGET_CARDS)
+	return tg and tg:IsExists(s.tfilter,1,nil,tp) and Duel.IsChainDisablable(ev)
+end
+function s.distg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_DISABLE,eg,1,0,0)
+	local rc=re:GetHandler()
+	if rc:IsDestructable() and rc:IsRelateToEffect(re) then
+		Duel.SetOperationInfo(0,CATEGORY_DESTROY,eg,1,0,0)
+	end
+end
+function s.disop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.NegateEffect(ev) and re:GetHandler():IsRelateToEffect(re) then
+		Duel.Destroy(eg,REASON_EFFECT)
 	end
 end
